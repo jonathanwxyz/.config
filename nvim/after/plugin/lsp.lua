2f09138133a20fe34447fb5f9beac783a2d4bd06
Jonathan <mail4jonathanw@gmail.com>
2023-07-25 21:18:40 +0100

    Update NVIM conf to use lazy.nvim and lsp-zero v3

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,55 +1,85 @@
-local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = {preserve_mappings = false},
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
+local lsp = require("lsp-zero").preset({})
+
+lsp.set_sign_icons({
+    error = "",
+    warn = "",
+    hint = "",
+    info = ""
 })
 
-lsp.set_preferences({
-    sign_icons = {
-        error = '',
-        warn = '',
-        hint = '',
-        info = ''
-    }
-})
-
-lsp.ensure_installed({
-    "tsserver",
-    "eslint",
-    "pyright",
-    "clangd",
-    "rust_analyzer",
-    "gopls",
-    "lua_ls"
-})
-
-lsp.setup_nvim_cmp({
-    preselect = 'none',
-    completion = {
-        completeopt = 'menu,menuone,noinsert,noselect'
-    },
-})
-
-lsp.nvim_workspace()
-
-
-lsp.on_attach(function(_, bufnr)
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({
+        buffer = bufnr,
+        preserve_mappings = false
+    })
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, {buffer = bufnr, remap = true})
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, { buffer = bufnr, remap = true })
 end)
 
-local lsp_util = require("lspconfig").util
-lsp.configure("denols", {
-    root_dir = lsp_util.root_pattern("deno.json", "deno.jsonc"),
+lsp.format_mapping("ff", {
+    format_opts = {
+        async = false,
+        timeout_ms = 10000,
+    },
+    servers = {
+        ["lua_ls"] = { "lua" },
+        ["rust_analyzer"] = { "rust" },
+        ["gopls"] = { "go" },
+        ["clangd"] = { "c", "c++" },
+        ["pyright"] = { "python" },
+        ["eslint"] = { "javascript", "typesript" },
+    }
 })
 
-lsp.configure("tsserver", {
-  root_dir = lsp_util.root_pattern("package.json, tsconfig.json"),
-  single_file_support = false
+-- Set up cmp
+require("lsp-zero").extend_cmp()
+
+local cmp = require("cmp")
+-- local cmp_action = require("lsp-zero").cmp_action()
+
+-- load external snippets
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip" },
+    },
+    mapping = {
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    },
 })
 
-lsp.setup()
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = {
+        "tsserver",
+        "eslint",
+        "pyright",
+        "clangd",
+        "rust_analyzer",
+        "gopls",
+        "lua_ls"
+    },
+    handlers = {
+        lsp.default_setup,
+        lua_ls = function()
+            require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+        end,
+    },
+})
+
+-- local lsp_util = require("lspconfig").util
+-- lsp.configure("denols", {
+--     root_dir = lsp_util.root_pattern("deno.json", "deno.jsonc"),
+-- })
+--
+-- lsp.configure("tsserver", {
+--   root_dir = lsp_util.root_pattern("package.json, tsconfig.json"),
+--   single_file_support = false
+-- })
--- nvim/lua/jonathan/plugins.lua
+++ nvim/lua/jonathan/plugins.lua
@@ -1,90 +1,98 @@
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim",
-    install_path })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
+vim.opt.rtp:prepend(lazypath)
 
-return require("packer").startup(function()
-  -- Packer can manage itself
-  use "wbthomason/packer.nvim"
-  -- plenary
-  use "nvim-lua/plenary.nvim"
-  -- telescope
-  use "nvim-telescope/telescope.nvim"
-  use {"nvim-telescope/telescope-fzf-native.nvim", run = "make",
-        requires = {"nvim-telescope/telescope.nvim"},
-        config = function() require("telescope").load_extension("fzf") end}
-  -- treesitter
-  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
-  use { "nvim-treesitter/playground"}
-  -- lsp
-  use {
-    "VonHeikemen/lsp-zero.nvim",
-    requires = {
-      -- LSP Support
-      {"neovim/nvim-lspconfig"},
-      {"williamboman/mason.nvim"},
-      {"williamboman/mason-lspconfig.nvim"},
+local plugins = {
+    -- plenary
+    "nvim-lua/plenary.nvim",
+    -- telescope
+    "nvim-telescope/telescope.nvim",
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        dependencies = { "nvim-telescope/telescope.nvim" },
+        config = function() require("telescope").load_extension("fzf") end
+    },
+    -- treesitter
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-treesitter/playground" },
+    -- lsp
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "dev-v3",
+        dependencies = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
-      -- Autocompletion
-      {"hrsh7th/nvim-cmp"},
-      {"hrsh7th/cmp-buffer"},
-      {"hrsh7th/cmp-path"},
-      {"saadparwaiz1/cmp_luasnip"},
-      {"hrsh7th/cmp-nvim-lsp"},
-      {"hrsh7th/cmp-nvim-lua"},
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
+            { "hrsh7th/cmp-cmdline" },
 
-      -- Snippets
-      {"L3MON4D3/LuaSnip"},
-      {"rafamadriz/friendly-snippets"},
-    }
-  }
-  --[[ -- cmp pictogram in autocomplete
-  use "onsails/lspkind.nvim" ]]
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        }
+    },
+    --[[ -- cmp pictogram in autocomplete
+  "onsails/lspkind.nvim" ]]
 
-  -- git
-  use {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end
-  }
-  use "tpope/vim-fugitive"
+    -- git
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup()
+        end
+    },
+    "tpope/vim-fugitive",
 
-  -- harpoon
-  use "ThePrimeagen/harpoon"
+    -- harpoon
+    "ThePrimeagen/harpoon",
 
-  -- commenting helper
-  use {
-    "numToStr/Comment.nvim",
-    config = function()
-      require('Comment').setup()
-    end
-  }
+    -- commenting helper
+    {
+        "numToStr/Comment.nvim",
+        config = function()
+            require('Comment').setup()
+        end
+    },
 
-  -- theming
-  use "navarasu/onedark.nvim"
-  use "nvim-lualine/lualine.nvim"
-  -- devicons
-  use "nvim-tree/nvim-web-devicons"
+    -- theming
+    "navarasu/onedark.nvim",
+    "nvim-lualine/lualine.nvim",
+    -- devicons
+    "nvim-tree/nvim-web-devicons",
 
-  -- undotree
-  use("mbbill/undotree")
+    -- undotree
+    "mbbill/undotree",
 
-  -- remember my mappings!
-  use {
-   "folke/which-key.nvim",
-   config = function()
-     require("which-key").setup {
-       -- your configuration comes here
-       -- or leave it empty to use the default settings
-       -- refer to the configuration section below
-     }
-    end
-  }
+    -- remember my mappings!
+    {
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to the default settings
+                -- refer to the configuration section below
+            }
+        end
+    },
+}
+local opts = {}
 
-  if packer_bootstrap then
-    require("packer").sync()
-  end
-end)
+require("lazy").setup(plugins, opts)
